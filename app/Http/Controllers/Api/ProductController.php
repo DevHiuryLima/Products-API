a13557c37d014ccc379aae8aa9b9304e6e54c8f5
Hiury Lima <limahiury6@gmail.com>
2023-10-06 16:02:10 -0300

    Configurando Base para API e Criando primeiro Endpoint

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    private Product $product;
+
+    public function __construct(Product $product)
+    {
+
+        $this->product = $product;
+    }
+
+    public function index() {
+        $products = $this->product->all();
+
+        return response()->json($products);
+    }
+}
