60f71bebede77ded721880784de4cbf01344c983
Hiury Lima <limahiury6@gmail.com>
2023-10-10 17:55:17 -0300

    Criando AbstractRepository.

--- app/Repository/ProductRepository.php
+++ app/Repository/ProductRepository.php
@@ -2,31 +2,6 @@
 
 namespace App\Repository;
 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
-
-class ProductRepository
+class ProductRepository extends AbstractRepository
 {
-    private Builder $model;
-
-    public function __construct(Builder $model)
-    {
-        $this->model = $model;
-    }
-
-    public function selectConditions($conditions) {
-        $expressions = explode(';', $conditions);
-        foreach ($expressions as $e) {
-            $exp = explode(':', $e);
-            $this->model = $this->model->where($exp[0], $exp[1], $exp[2]);
-        }
-    }
-
-    public function selectFilter($filters) {
-        $this->model = $this->model->selectRaw($filters);
-    }
-
-    public function getResult(){
-        return $this->model;
-    }
 }
