1ecaad9adad7068a348530661de11c92041f6a0b
Hiury Lima <limahiury6@gmail.com>
2023-10-10 08:28:05 -0300

    Criando a ProductFactory.php e ProductTableSeeder.php.

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'price' => $this->faker->randomFloat(2, 0,  8),
+            'description' => $this->faker->text,
+            'slug' => $this->faker->slug,
+        ];
+    }
+}
--- database/seeders/ProductTableSeeder.php
+++ database/seeders/ProductTableSeeder.php
@@ -5,16 +5,15 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ProductTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(ProductTableSeeder::class);
+        \App\Models\Product::factory(30)->create();
     }
 }
